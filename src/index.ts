0db132eff44c35bdd6f244d0c488a66565ecf60e
Alan Vinicius Silva <alanvinicios@gmail.com>
2020-03-02 04:55:42 -0300

    End-Of-Day Historical Data

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
 
 const BASE_URL = 'https://eodhistoricaldata.com/api';
 const PATH_REGEX = /^(\/[^/]+)*$/;
+const SYMBOL_REGEX = /(\w+)(\.)(\w+)/;
 
 
 async function request(path: string, data: any): Promise<any>{
@@ -11,7 +12,7 @@ async function request(path: string, data: any): Promise<any>{
             const config: AxiosRequestConfig = {
                 method: 'get',
                 url: url,
-                data
+                data: data || {}
             };
             axios(config).then((value: AxiosResponse<any>) => resolve(value.data)).catch(reject);
         }
@@ -22,20 +23,40 @@ async function request(path: string, data: any): Promise<any>{
 }
 
 type ExgCode = "AS" | "AT" | "BA" | "BE" | "BK" | "BOND" | "BR" | "BSE" | "BUD" | "CC" | "CO" | "COMM" | "DU" | "EUFUND" | "F" | "FOREX" | "HA" | "HE" | "HK" | "HM" | "IC" | "IL" | "INDX" | "IR" | "JK" | "JSE" | "KAR" | "KLSE" | "KO" | "KQ" | "LS" | "LSE" | "LU" | "MC" | "MI" | "MU" | "MX" | "NB" | "NFN" | "NSE" | "OL" | "PA" | "PSE" | "RUFUND" | "SA" | "SG" | "SHE" | "SHG" | "SN" | "SR" | "ST" | "STU" | "SW" | "TA" | "TO" | "TSE" | "TW" | "TWO" | "US" | "V" | "VI" | "VN" | "VX" | "WAR" | "XETRA";
+type Fmt = "json" | "csv";
+type Period = "d" | "w" | "m";
+type Options = {api_token?: string, from?: string, to?: string, period?: Period, filter?: string, fmt?: Fmt}
 
-class EODData {
+class EodApi {
 
-    apiToken: String = "";
-    exchangeCode?: String;
-
+    apiToken: string = "";
+    exchangeCode: string = "";
+    
     setup(config: {apiToken: string, exchangeCode?: ExgCode}){
         if(!config) throw new Error("You must provide config");
         if(!config.apiToken) throw new Error("You must provide your API token");
-
+        if(!config.apiToken.length) throw new Error("You must provide a valid API token")
         this.apiToken = config.apiToken;
-        this.exchangeCode = config.exchangeCode;
+        this.exchangeCode = config.exchangeCode?`.${config.exchangeCode}`:"";
         return this;
+    }
+       
+    // End-Of-Day Historical Data
+    async eod(symbols: string[] | string, options?: Options){
+        if(this.apiToken.length === 0) throw new Error("You need to use the setup funcion before call any function");
+
+        if(options)
+            options.api_token = this.apiToken;
+        else
+            options = {api_token: this.apiToken};
+
+        let path = "/eod"
+        if(typeof symbols === "string"){
+            path += `/${symbols}`
+            path += this.exchangeCode;
+            return await request(path, options);
+        }
     }
 }
 
-export const eodData = new EODData();
+export const eodData = new EodApi();
--- test/test.js
+++ test/test.js
@@ -1,6 +1,8 @@
 const {eodData} = require('../dist/index');
 
-eodData.setup({apiToken: "OeAFFmMliFG5orCUuwAKQ8l4WWFQ67YX", exchangeCode: "SA"});
+eodData.setup({apiToken: "OeAFFmMliFG5orCUuwAKQ8l4WWFQ67YX"});
 
-
-console.log(eodData);
+(async () => {
+    const res = await eodData.eod("AAPL.US", {fmt: 'json', from: "2020-02-01"});
+    console.log(res);
+})();
